fb822587b0d96204a607543a00872a3e4b88c903
Haider Abbas <haiderabbas@Haiders-MacBook-Air.local>
2021-08-06 20:58:47 +0500

    built SmallCard component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import SmallCard from "../components/SmallCard";
 
-export default function Home() {
+export default function Home({ exploreData }) {
 	return (
 		<div className="">
 			<Head>
@@ -14,6 +15,20 @@ export default function Home() {
 			<main className="max-w-7xl mx-auto px-8 sm:px-16">
 				<section className="pt-6">
 					<h2 className="text-4xl font-semibold pb-5">Explore nearby</h2>
+
+					<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+						{exploreData?.map(({ img, distance, location }) => (
+							<SmallCard
+								key={img} //wheneer use map, use a key. it avoids rendering entire list when we add something new
+								img={img}
+								distance={distance}
+								location={location}
+							></SmallCard>
+						))}
+					</div>
+				</section>
+				<section>
+					<h2 className="text-4xl font-semibold pb-5 py-8">Live Anywhere</h2>
 				</section>
 			</main>
 		</div>
