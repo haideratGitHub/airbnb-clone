3048466ce587d10aef8c88d0f32d4c9884c50524
haiderAtGitHub <haiderabbas1982000@gmail.com>
2021-08-10 23:04:20 +0500

    Added progress bar

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -10,7 +10,12 @@ function InfoCard({ img, location, title, description, star, price, total }) {
         transition duration-200 ease-out first:border-t"
 		>
 			<div className="relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0">
-				<Image src={img} layout="fill" objectFit="cover"></Image>
+				<Image
+					src={img}
+					layout="fill"
+					objectFit="cover"
+					className="rounded-2xl"
+				></Image>
 			</div>
 			<div className="flex flex-col flex-grow pl-5">
 				<div className="flex justify-between">
